a9f20a046df5f7282221a73faf28bf8f096e2c2c
Arsenii <yarmolinskiyam@gmail.com>
2026-04-03 15:13:39 +0300

    feat: driver coeffs setup on startup

--- src/kernel/kernel.hpp
+++ src/kernel/kernel.hpp
@@ -6,14 +6,40 @@
 namespace kernel
 {
 
+void error(const char* msg, ...);
+
+void reboot();
+
+void progress_start();
+float progress_tick(float progress);
+void progress_finish();
+
 #define kerror(msg, ...) logError(msg, ##__VA_ARGS__)
 #define kwarning(msg, ...) logWarning(msg, ##__VA_ARGS__)
 #define kinfo(msg, ...) logInfo(msg, ##__VA_ARGS__)
 #define kdebug(msg, ...) logDebug(msg, ##__VA_ARGS__)
 #define kverbose(msg, ...) logVerbose(msg, ##__VA_ARGS__)
 
-void error(const char* msg, ...);
+#define kprogress_start(msg, ...)                                                                \
+    if (LOG_ENABLE)                                                                              \
+    {                                                                                            \
+        kernel::progress_start();                                                                \
+        logWrite(LOG_ALL_OBJ, LOG_DEBUG, DEBUG_TEXT " " msg "" LOG_END, LOG_TIME_STAMP, LOG_TAG, \
+                 ##__VA_ARGS__);                                                                 \
+    }
 
-void reboot();
+#define kprogress_tick(progress)                                        \
+    if (LOG_ENABLE)                                                     \
+    {                                                                   \
+        float current = kernel::progress_tick(progress);                \
+        logWrite(LOG_ALL_OBJ, LOG_DEBUG, "...%.1f%%" LOG_END, current); \
+    }
+
+#define kprogress_finish(msg, ...)                                       \
+    if (LOG_ENABLE)                                                      \
+    {                                                                    \
+        logWrite(LOG_ALL_OBJ, LOG_DEBUG, msg "" LOG_END, ##__VA_ARGS__); \
+        kernel::progress_finish();                                       \
+    }
 
 }  // namespace kernel
