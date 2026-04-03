ad45c73ac682706997811ec51bec1140e8815e6d
Arsenii <yarmolinskiyam@gmail.com>
2026-04-03 12:59:05 +0300

    patch: use halffloat for chassis control

--- src/devices/bldc/bldc.cpp
+++ src/devices/bldc/bldc.cpp
@@ -1,6 +1,7 @@
 #include "bldc.hpp"
 
 #include "devices/device_manager.hpp"
+#include "half_float.h"
 
 namespace devices::bldc
 {
@@ -11,21 +12,27 @@ void BldcsDriver::motorWrite(uint8_t motor_addr, uint8_t register_addr, uint8_t 
     can->write(id, data, len);
 }
 
+void BldcsDriver::setDriveRegisterFloat(uint8_t register_addr, float value)
+{
+    motorWrite(0, register_addr, (uint8_t *)&value, 4);
+}
+
 void BldcsDriver::setDriveVel(BldcsVec vel)
 {
+    uint8_t payload[8] = {0};
+
     for (size_t i = 0; i < 4; i++)
     {
-        union
-        {
-            float value;
-            uint8_t buffer[4];
-        } target_speed;
-        target_speed.value = -vel.vec[i];  // Positive speed should be CCW viewed from the top
-        motorWrite(i + 1, 0x0A, target_speed.buffer, 4);
+        float value = -vel.vec[i];  // Positive speed should be CCW viewed from the top
+        ushort value_fp16 = float_to_half(value);
+        reinterpret_cast<uint16_t *>(payload)[i] = value_fp16;
     }
+
+    motorWrite(0, 96, payload, 8);
 
     kdebug("setDriveVel: %f %f %f %f", vel.vec[0], vel.vec[1], vel.vec[2], vel.vec[3]);
 }
+
 void BldcsDriver::getDriveVel(BldcsVec *vel)
 {
     kdebug("getDriveVel: %f %f %f %f", vel->vec[0], vel->vec[1], vel->vec[2], vel->vec[3]);
