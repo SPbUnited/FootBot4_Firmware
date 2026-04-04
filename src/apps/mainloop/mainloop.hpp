357476f3b2b9a980c1b66f052e6335be69cc2d8d
Arsenii <yarmolinskiyam@gmail.com>
2026-04-04 20:49:39 +0300

    feat: local odometry

--- src/apps/mainloop/mainloop.hpp
+++ src/apps/mainloop/mainloop.hpp
@@ -5,18 +5,8 @@
 namespace apps::mainloop
 {
 
-struct MainloopState
-{
-    uint32_t deltamin;
-    uint32_t deltaavg;
-    uint32_t deltamax;
-    uint32_t counter;
-};
-
 bool is_loop_pending(uint32_t time_elapsed, uint32_t period);
 
 void loop();
-
-MainloopState get_state();
 
 }  // namespace apps::mainloop
