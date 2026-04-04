d6ab716a3a786615c2e03bf57fbf3d57275aa8cf
Arsenii <yarmolinskiyam@gmail.com>
2026-04-04 14:41:19 +0300

    feat: process monitoring and top utility

--- src/kernel/scheduler/scheduler.hpp
+++ src/kernel/scheduler/scheduler.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "inttypes.h"
+#include "stddef.h"
 
 namespace kernel::scheduler
 {
@@ -11,17 +12,58 @@ enum TaskState
     running,
 };
 
+struct MinAvgMax
+{
+    uint32_t min;
+    uint32_t avg;
+    uint32_t max;
+
+    void update(uint32_t value)
+    {
+        if (min == 0 || value < min)
+        {
+            min = value;
+        }
+        if (value > max)
+        {
+            max = value;
+        }
+        if (avg < min)
+        {
+            avg = min;
+        }
+        if (avg > max)
+        {
+            avg = max;
+        }
+        avg = (avg * 0.9 + value * 0.1);
+    }
+};
+
 struct TaskDescriptor
 {
+    char name[20];
     void (*function)();
     uint32_t last_exec;
     uint32_t period;
     bool (*is_pending)(uint32_t time_elapsed, uint32_t period);
     TaskState state;
+    struct TaskMonitor
+    {
+        MinAvgMax exec_time;
+        MinAvgMax period_time;
+        uint32_t counter;
+    } monitor;
 };
 
 void yield();
 
 void init();
+
+void delay_ms(uint32_t delay_ms);
+void delay_us(uint32_t delay_us);
+
+TaskDescriptor *get_tasks();
+size_t get_task_count();
 
 }  // namespace kernel::scheduler
---
+++ src/kernel/scheduler/scheduler.shell.cpp
@@ -0,0 +1,62 @@
+#include "scheduler.hpp"
+
+#include "devices/device_manager.hpp"
+#include "devices/shell/shell.main.hpp"
+
+int schedulerTop()
+{
+    Shell *ch_shell = shellGetCurrent();
+    if (!ch_shell)
+    {
+        return -1;
+    }
+
+    kernel::scheduler::TaskDescriptor *tasks = kernel::scheduler::get_tasks();
+
+    printf("\033[2J");  // Clear screen
+
+    while (true)
+    {
+        char c = getchar();
+        if (c == -1)
+        {
+            printf("\nError! Cannot read keyboard input!");
+            break;
+        }
+        if (c == 'q')
+        {
+            break;
+        }
+        else if (c == 'r')
+        {
+            for (size_t app = 0; app < kernel::scheduler::get_task_count(); app++)
+            {
+                tasks[app].monitor.exec_time = {0};
+                tasks[app].monitor.period_time = {0};
+                tasks[app].monitor.counter = 0;
+            }
+        }
+        // printf("\033[2J\033[H\n");  // Clear screen and move cursor to home position
+        printf("\033[H\n");
+        printf(
+            "app |                name  |   exec_time (min|avg|max)  |  period_time (min|avg|max) "
+            "| "
+            "counter \n");
+        for (size_t app = 0; app < kernel::scheduler::get_task_count(); app++)
+        {
+            printf(" %2d | %20s | %8lu|%8lu|%8lu | %8lu|%8lu|%8lu | %6lu \n", app, tasks[app].name,
+                   tasks[app].monitor.exec_time.min, tasks[app].monitor.exec_time.avg,
+                   tasks[app].monitor.exec_time.max, tasks[app].monitor.period_time.min,
+                   tasks[app].monitor.period_time.avg, tasks[app].monitor.period_time.max,
+                   tasks[app].monitor.counter);
+        }
+
+        printf("\nPress 'q' to quit, 'r' to reset monitor.\n");
+
+        kernel::scheduler::delay_ms(250);
+    }
+
+    return 0;
+}
+SHELL_EXPORT_CMD(SHELL_CMD_PERMISSION(0) | SHELL_CMD_TYPE(SHELL_TYPE_CMD_MAIN), top, schedulerTop,
+                 show scheduler info);
