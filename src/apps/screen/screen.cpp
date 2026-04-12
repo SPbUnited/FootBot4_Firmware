541de9d524013f2712f27b97f465c8cb2038e02d
Arsenii <yarmolinskiyam@gmail.com>
2026-04-12 23:46:28 +0300

    patch: added confirmation on save (you need to hold the button to save config)

--- src/apps/screen/screen.cpp
+++ src/apps/screen/screen.cpp
@@ -13,6 +13,9 @@ namespace apps::screen
 
 void screen()
 {
+    // static uint32_t last_click = 0;
+    // static bool is_updating = true;
+
     static int counter = 0;
     int version_len = strlen(FIRMWARE_VERSION);
     int version_field_width = 10;
@@ -44,7 +47,6 @@ void screen()
     display.printf("robot  %2d |%s---------------------x: %6.3f |y: %6.3f\ntheta: %6.3f",
                    devices::robot_dev.robot_id, vbuf, offset, devices::robot_dev.current_pos.x,
                    devices::robot_dev.current_pos.y, devices::robot_dev.current_pos.theta);
-    display.flush();
 
     counter++;
     if (counter > a + a + t1 + t2)
@@ -72,7 +74,33 @@ void screen()
     {
         devices::robot_dev.dec_id();
     }
-    if (!drivers::gpio_in.read(drivers::gpio::BUTTON_SELECT) && old_select)
+
+    static int save_progress = 0;
+
+    if (drivers::gpio_in.read(drivers::gpio::BUTTON_SELECT))
+    {
+        save_progress++;
+    }
+    else
+    {
+        save_progress = 0;
+    }
+
+    if (save_progress != 0)
+    {
+        display.clear();
+        display.setCursor(0, 0);
+        display.setTextSize(1);
+        display.printf(" Hold the button to\nsave config to EEPROM\n");
+        display.printf("     [");
+        for (int i = 0; i < 10; i++)
+        {
+            display.printf("%c", i <= save_progress ? '#' : ' ');
+        }
+        display.printf("]");
+    }
+
+    if (save_progress > 10)
     {
         kinfo("Saving robot config to EEPROM");
         display.clear();
@@ -88,6 +116,27 @@ void screen()
     old_up = drivers::gpio_in.read(drivers::gpio::BUTTON_ADDR_UP);
     old_down = drivers::gpio_in.read(drivers::gpio::BUTTON_ADDR_DOWN);
     old_select = drivers::gpio_in.read(drivers::gpio::BUTTON_SELECT);
+
+    // if (old_up || old_down || old_select)
+    // {
+    //     last_click = drivers::system_clock::micros();
+    // }
+
+    // if (drivers::system_clock::micros() - last_click < 10000000)
+    // {
+    //     display.flush();
+    //     is_updating = true;
+    // }
+    // else
+    // {
+    //     if (is_updating)
+    //     {
+    //         display.clear();
+    //         display.flush();
+    //         is_updating = false;
+    //     }
+    // }
+    display.flush();
 }
 
 }  // namespace apps::screen
