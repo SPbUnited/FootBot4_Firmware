de0d3889ce8a005ac1e2d96f954f4d6cbf0d4d94
Arsenii <yarmolinskiyam@gmail.com>
2026-04-13 11:43:50 +0300

    refactor: gpio driver separate for each pin

--- src/drivers/driver_manager.cpp
+++ src/drivers/driver_manager.cpp
@@ -144,6 +144,26 @@ buzzer::Buzzer buzzer_drv;
 bootstrap::Bootstrap bootstrap_drv;
 system_clock::SystemClock system_clock_drv;
 
+gpio::GPIOOutputDriver out_pins[OUT_COUNT];
+gpio::GPIOInputDriver in_pins[INPUT_COUNT];
+
+gpio::GPIODescriptor out_pins_desc[] = {
+    [LED_STM32] = {GPIOD, GPIO_PIN_15},
+    [LED_DRV1] = {GPIOG, GPIO_PIN_2},
+    [LED_DRV2] = {GPIOG, GPIO_PIN_3},
+    [LED_DRV3] = {GPIOG, GPIO_PIN_4},
+    [LED_DRV4] = {GPIOG, GPIO_PIN_5},
+    [LED_DRV5] = {GPIOG, GPIO_PIN_6},
+    [LED_DATA_TRANSFER_STATUS_1] = {GPIOG, GPIO_PIN_7},
+    [LED_DATA_TRANSFER_STATUS_2] = {GPIOG, GPIO_PIN_8},
+};
+
+gpio::GPIODescriptor in_pins_desc[] = {
+    [BUTTON_ADDR_UP] = {GPIOE, GPIO_PIN_11},
+    [BUTTON_ADDR_DOWN] = {GPIOE, GPIO_PIN_10},
+    [BUTTON_SELECT] = {GPIOE, GPIO_PIN_9},
+};
+
 void init()
 {
     drivers::system_clock_drv.init();
@@ -153,8 +173,19 @@ void init()
 
     drivers::uart4.init();
     drivers::i2c2.init();
-    drivers::gpio_out.init();
-    drivers::gpio_in.init();
+
+    __HAL_RCC_GPIOG_CLK_ENABLE();
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+    __HAL_RCC_GPIOE_CLK_ENABLE();
+    for (int i = 0; i < OUT_COUNT; i++)
+    {
+        out_pins[i].init(out_pins_desc[i]);
+    }
+    for (int i = 0; i < INPUT_COUNT; i++)
+    {
+        in_pins[i].init(in_pins_desc[i]);
+    }
+
     drivers::can_drv.init();
     drivers::nrf24_drv.init();
 }
