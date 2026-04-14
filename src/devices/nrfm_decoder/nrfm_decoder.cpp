26d4a45041f93468dad7f2dd087178ed62760602
Arsenii <yarmolinskiyam@gmail.com>
2026-04-14 21:17:40 +0300

    patch: nrf speedup and phantom packets fix

--- src/devices/nrfm_decoder/nrfm_decoder.cpp
+++ src/devices/nrfm_decoder/nrfm_decoder.cpp
@@ -15,6 +15,34 @@ void nrfm_rx_callback(uint8_t *data, uint8_t len)
         return;
     }
 
+    bool is_phantom_packet = true;
+
+    for (size_t i = 1; i < 4; i++)
+    {
+        if (packet->data[i] != packet->data[0])
+        {
+            is_phantom_packet = false;
+            break;
+        }
+    }
+
+    if (is_phantom_packet)
+    {
+        // kerror(
+        //     "Detected phantom packet: [%02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X "
+        //     "%02X %02X %02X %02X "
+        //     "%02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X]",
+        //     packet->data[0], packet->data[1], packet->data[2], packet->data[3], packet->data[4],
+        //     packet->data[5], packet->data[6], packet->data[7], packet->data[8], packet->data[9],
+        //     packet->data[10], packet->data[11], packet->data[12], packet->data[13],
+        //     packet->data[14], packet->data[15], packet->data[16], packet->data[17],
+        //     packet->data[18], packet->data[19], packet->data[20], packet->data[21],
+        //     packet->data[22], packet->data[23], packet->data[24], packet->data[25],
+        //     packet->data[26], packet->data[27], packet->data[28], packet->data[29],
+        //     packet->data[30], packet->data[31]);
+        return;
+    }
+
     kverbose(
         "nrfm_packet: [%02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X "
         "%02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X]",
@@ -73,14 +101,14 @@ void nrfmTestOldPacket(int8_t velx, int8_t vely, int8_t angular_velocity_or_angl
                                .angular_velocity_or_angle = angular_velocity_or_angle,
                                .dribbler_setting = dribbler_setting,
                                .kicker_setting = kicker_setting,
-                               .reserved = flags & 0b10000000,
-                               .AF = flags & 0b01000000,
-                               .AU = flags & 0b00100000,
-                               .KF = flags & 0b00010000,
-                               .KU = flags & 0b00001000,
-                               .AS = flags & 0b00000100,
-                               .DE = flags & 0b00000010,
                                .HE = flags & 0b00000001,
+                               .DE = flags & 0b00000010,
+                               .AS = flags & 0b00000100,
+                               .KU = flags & 0b00001000,
+                               .KF = flags & 0b00010000,
+                               .AU = flags & 0b00100000,
+                               .AF = flags & 0b01000000,
+                               .reserved = flags & 0b10000000,
                            },
                    }}};
 
@@ -103,14 +131,14 @@ void nrfmTestNewOldPacket(int8_t velx, int8_t vely, int8_t angular_velocity_or_a
                                .angular_velocity_or_angle = angular_velocity_or_angle,
                                .dribbler_setting = dribbler_setting,
                                .kicker_setting = kicker_setting,
-                               .reserved = flags & 0b10000000,
-                               .AF = flags & 0b01000000,
-                               .AU = flags & 0b00100000,
-                               .KF = flags & 0b00010000,
-                               .KU = flags & 0b00001000,
-                               .AS = flags & 0b00000100,
-                               .DE = flags & 0b00000010,
                                .HE = flags & 0b00000001,
+                               .DE = flags & 0b00000010,
+                               .AS = flags & 0b00000100,
+                               .KU = flags & 0b00001000,
+                               .KF = flags & 0b00010000,
+                               .AU = flags & 0b00100000,
+                               .AF = flags & 0b01000000,
+                               .reserved = flags & 0b10000000,
                            },
                    }}};
 
